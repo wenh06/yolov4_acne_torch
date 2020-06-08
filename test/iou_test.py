fix errors in computation of alpha of ciou

test/iou_test.py
@@ -5,6 +5,7 @@
 import torch
 import os, sys
 from torch.nn import functional as F
+from easydict import EasyDict as ED
 
 import numpy as np
 
@@ -15,6 +16,8 @@ def bboxes_iou_test(bboxes_a, bboxes_b, fmt='voc', iou_type='iou'):
     """
     if 'plt' not in dir():
         import matplotlib.pyplot as plt
+    if 'cv2' not in dir():
+        import cv2
     
     assert iou_type.lower() in ['iou', 'giou', 'diou', 'ciou']
 
@@ -49,12 +52,6 @@ def bboxes_iou_test(bboxes_a, bboxes_b, fmt='voc', iou_type='iou'):
     area_a = torch.prod(bb_a, 1)
     area_b = torch.prod(bb_b, 1)
 
-    print(f"area_a = {area_a}")
-    print(f"area_b = {area_b}")
-
-    if N==K==1:
-        ba, bb = bboxes_a[0], bboxes_b[0]
-    
     # torch.prod(input, dim, keepdim=False, dtype=None) → Tensor
     # Returns the product of each row of the input tensor in the given dimension dim
     # if tl, br does not form a nondegenerate squre, then the corr. element in the `prod` would be 0
@@ -65,8 +62,8 @@ def bboxes_iou_test(bboxes_a, bboxes_b, fmt='voc', iou_type='iou'):
 
     iou = torch.true_divide(area_intersect, area_union)
 
-    if iou_type.lower() == 'iou':
-        return iou
+    # if iou_type.lower() == 'iou':
+    #     return iou
 
     if fmt.lower() == 'voc':  # xmin, ymin, xmax, ymax
         # top left
@@ -88,8 +85,12 @@ def bboxes_iou_test(bboxes_a, bboxes_b, fmt='voc', iou_type='iou'):
 
     giou = iou - (area_covering - area_union) / area_covering
 
-    if iou_type.lower() == 'giou':
-        return giou
+    print(f"tl_union.shape = {tl_union.shape}")
+    print(f"br_union.shape = {br_union.shape}")
+    print(f"bboxes_c.shape = {bboxes_c.shape}")
+
+    # if iou_type.lower() == 'giou':
+    #     return giou
 
     if fmt.lower() == 'voc':  # xmin, ymin, xmax, ymax
         centre_a = (bboxes_a[..., 2 :] + bboxes_a[..., : 2]) / 2
@@ -103,16 +104,85 @@ def bboxes_iou_test(bboxes_a, bboxes_b, fmt='voc', iou_type='iou'):
 
     diou = iou - centre_dist.pow(2) / diag_len.pow(2)
 
-    if iou_type.lower() == 'diou':
-        return diou
+    # if iou_type.lower() == 'diou':
+    #     return diou
 
     # bb_a of shape `(N,2)`, bb_b of shape `(K,2)`
     v = torch.einsum('nm,km->nk', bb_a, bb_b)
     v = torch.true_divide(v, (torch.norm(bb_a, p='fro', dim=1)[:,np.newaxis] * torch.norm(bb_b, p='fro', dim=1)))
     v = torch.true_divide(2*torch.acos(v), np.pi).pow(2)
-    alpha = (iou>=0.5).type(iou.type())
+    alpha = (torch.true_divide(v, 1-iou+v))*((iou>=0.5).type(iou.type()))
 
     ciou = diou - alpha * v
 
+    if N==K==1:
+        print("\n"+"*"*50)
+        print(f"bboxes_a = {bboxes_a}")
+        print(f"bboxes_b = {bboxes_b}")
+
+        print(f"area_a = {area_a}")
+        print(f"area_b = {area_b}")
+
+        print(f"area_intersect = {area_intersect}")
+        print(f"area_union = {area_union}")
+
+        print(f"tl_intersect = {tl_intersect}")
+        print(f"br_intersect = {br_intersect}")
+        print(f"tl_union = {tl_union}")
+        print(f"br_union = {br_union}")
+
+        print(f"area_covering (area of bboxes_c) = {area_covering}")
+        
+        print(f"centre_dist = {centre_dist}")
+        print(f"diag_len = {diag_len}")
+
+        print("for computing ciou")
+        print(f"v = {v}")
+        print(f"alpha = {alpha}")
+
+        bc = ED({"xmin":tl_union.numpy().astype(int)[0][0][0], "ymin":tl_union.numpy().astype(int)[0][0][1], "xmax":br_union.numpy().astype(int)[0][0][0], "ymax":br_union.numpy().astype(int)[0][0][1]})
+        adjust_x = bc.xmin - int(0.25*(bc.xmax-bc.xmin))
+        adjust_y = bc.ymin - int(0.25*(bc.ymax-bc.ymin))
+
+        print(f"adjust_x = {adjust_x}")
+        print(f"adjust_y = {adjust_y}")
+
+        bc.xmin, bc.ymin, bc.xmax, bc.ymax = bc.xmin-adjust_x, bc.ymin-adjust_y, bc.xmax-adjust_x, bc.ymax-adjust_y
+        
+        ba, bb = bboxes_a.numpy().astype(int)[0], bboxes_b.numpy().astype(int)[0]
+        if fmt.lower() == 'voc':  # xmin, ymin, xmax, ymax
+            ba = ED({"xmin":ba[0]-adjust_x, "ymin":ba[1]-adjust_y, "xmax":ba[2]-adjust_x, "ymax":ba[3]-adjust_y})
+            bb = ED({"xmin":bb[0]-adjust_x, "ymin":bb[1]-adjust_y, "xmax":bb[2]-adjust_x, "ymax":bb[3]-adjust_y})
+        elif fmt.lower() == 'coco':  # xmin, ymin, w, h
+            ba = ED({"xmin":ba[0]-adjust_x, "ymin":ba[1], "xmax":ba[0]+ba[2]-adjust_x, "ymax":ba[1]+ba[3]-adjust_y})
+            bb = ED({"xmin":bb[0]-adjust_x, "ymin":bb[1], "xmax":bb[0]+bb[2]-adjust_x, "ymax":bb[1]+bb[3]-adjust_y})
+
+        print(f"ba = {ba}")
+        print(f"bb = {bb}")
+        print(f"bc = {bc}")
+
+        plane = np.full(shape=(int(1.5*(bc.ymax-bc.ymin)),int(1.5*(bc.xmax-bc.xmin)),3), fill_value=255, dtype=np.uint8)
+        img_with_boxes = plane.copy()
+
+        line_size = 1
+        cv2.rectangle(img_with_boxes, (ba.xmin, ba.ymin), (ba.xmax, ba.ymax), (0, 255, 0), line_size)
+        cv2.rectangle(img_with_boxes, (bb.xmin, bb.ymin), (bb.xmax, bb.ymax), (0, 0, 255), line_size)
+        cv2.rectangle(img_with_boxes, (bc.xmin, bc.ymin), (bc.xmax, bc.ymax), (255, 0, 0), line_size)
+
+        plt.figure(figsize=(7,7))
+        plt.imshow(img_with_boxes)
+        plt.show()
+
+        print(f"iou = {iou}")
+        print(f"giou = {giou}")
+        print(f"diou = {diou}")
+        print(f"ciou = {ciou}")
+
     if iou_type.lower() == 'ciou':
         return ciou
+    elif iou_type.lower() == 'diou':
+        return diou
+    elif iou_type.lower() == 'giou':
+        return giou
+    elif iou_type.lower() == 'iou':
+        return iou

train_acne.py
@@ -121,7 +121,7 @@ def bboxes_iou(bboxes_a, bboxes_b, fmt='voc', iou_type='iou'):
     v = torch.einsum('nm,km->nk', bb_a, bb_b)
     v = torch.true_divide(v, (torch.norm(bb_a, p='fro', dim=1)[:,np.newaxis] * torch.norm(bb_b, p='fro', dim=1)))
     v = torch.true_divide(2*torch.acos(v), np.pi).pow(2)
-    alpha = (iou>=0.5).type(iou.type())
+    alpha = (torch.true_divide(v, 1-iou+v))*((iou>=0.5).type(iou.type()))
 
     ciou = diou - alpha * v
 